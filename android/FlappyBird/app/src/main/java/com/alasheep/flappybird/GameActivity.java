b7c3e1e1a873645c49fc9ea4a3efc9660f1bd5b7
soongil kim <alasheep@gmail.com>
2019-06-16 19:46:36 +0900

    first code about flappy bird

---
+++ android/FlappyBird/app/src/main/java/com/alasheep/flappybird/GameActivity.java
@@ -0,0 +1,12 @@
+package com.alasheep.flappybird;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class GameActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
